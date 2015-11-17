436a4def8b1dd426b9ab4cf4bc287f8e60f14a77
Tin Huynh <tinlun123@gmail.com>
2015-11-16 23:51:17 -0800

    Added basic platformer movements.
    Has collision detection and gravity and everything.

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+    public float speed;
+    public float jumpStrength;
+    public Transform groundChecker;  // A point on the player, used to check for ground.
+    public LayerMask groundLayerMask;  // Only check for ground using ground, and nothing else.
+    
+    private float groundCheckerRadius;  // How large of a circle the checker is.
+    private bool grounded;  // Whether the player is on the ground, for jump checking.
+    private Rigidbody2D rigidbody2D;
+
+
+	void Start () {
+        // Get the player's rigidbody2D component for movement uses.
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        groundCheckerRadius = GetComponent<CircleCollider2D>().radius;
+        print(groundCheckerRadius);
+	}
+	
+
+	// Update is called once per frame.
+	void Update () {
+        // Make player move left or right, depending on which key is pressed.
+        rigidbody2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, rigidbody2D.velocity.y);
+
+        grounded = Physics2D.OverlapCircle(groundChecker.position, groundCheckerRadius, groundLayerMask);
+        print(grounded);
+        if (grounded && Input.GetButton("Jump")) {
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpStrength);
+            grounded = false;
+        }
+	}
+}
