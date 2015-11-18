d76afcff5052119ea2c9d95b0d5772be733148b7
Tin Huynh <tinlun123@gmail.com>
2015-11-17 23:57:53 -0800

    WIP particle system
    I can now script individual particles but the way it works is kinda
    weird - needs more experimentation.

---
+++ Assets/Scripts/ParticleChaseScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleChaseScript : MonoBehaviour {
+    public Transform target;  // The target for the particles to chase.
+
+    private ParticleSystem.Particle[] particleList;
+    private ParticleSystem particleSystem;  // The particle system 
+
+	// Use this for initialization
+	void Start () {
+        particleSystem = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        particleList = new ParticleSystem.Particle[particleSystem.particleCount];
+        particleSystem.GetParticles(particleList);
+        for(int i = 0; i < particleList.Length; i ++){
+            particleList[i].velocity = new Vector3((target.position.x - particleList[i].position.x), 0, target.position.z - particleList[i].position.z);
+            //particleList[i].velocity = new Vector3(15, 15, 0);
+        }
+        particleSystem.SetParticles(particleList, particleList.Length);
+	}
+}
