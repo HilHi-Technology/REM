3710961ca98aef81dfc89efb75f98f6cf94b4ac9
mylies43 <awesomesauce712@gmail.com>
2015-11-19 18:41:30 -0800

    Added Blur and Color
    If I screw up again backup from here

---
+++ Assets/MouseChanger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseChanger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	float x = Input.mousePosition.x;
+	
+	if(x >= Screen.width/2)
+	{
+		GetComponent<SpriteRenderer>().color = Color.red;
+
+	}
+		else
+		{
+			GetComponent<SpriteRenderer>().color = Color.blue;
+
+		}	
+	
+	}
+}
