47ae754fcd97e5d288595c6ed2db525ce05bf6d0
mylies43 <awesomesauce712@gmail.com>
2015-11-19 18:31:50 -0800

    Added a menu
    Pretty simple added a menu with a non-functioning button

---
+++ Assets/MouseChaser.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseChaser : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	float x = Input.mousePosition.x;
+	
+	print(x);
+	}
+}
